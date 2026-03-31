133c28a4409e123e7e7ac33c94b638138dde948f
Husen <hi@husen.id>
2026-03-31 21:26:10 +0700

    chore: use built in font config
    Signed-off-by: Husen <hi@husen.id>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -9,16 +8,45 @@ export default defineConfig({
   site: process.env.ASTRO_SITE || "http://localhost:4321",
 
   image: {
-      domains: ['gravatar.com'],
-      responsiveStyles: true,
-  layout: 'constrained',
+    domains: ['gravatar.com'],
+    responsiveStyles: true,
+  	layout: 'constrained',
 	},
 
   integrations: [
-      sitemap(),
+    sitemap(),
 	],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  fonts: [
+  	{
+  		provider: fontProviders.fontsource(),
+  		name: 'Lexend Deca',
+  		cssVariable: '--font-lexend-deca',
+  		weights: [200, 400, 600, 700],
+  	},
+		{
+  		provider: fontProviders.fontsource(),
+  		name: 'JetBrains Mono',
+  		cssVariable: '--font-jetbrains-mono',
+  		weights: [200, 400, 600, 700],
+  	},
+		{
+  		provider: fontProviders.fontsource(),
+  		name: 'Barlow',
+  		cssVariable: '--font-barlow',
+  		weights: [400, 700],
+  		formats: ['woff']
+  	},
+		{
+  		provider: fontProviders.fontsource(),
+  		name: 'Outfit',
+  		cssVariable: '--font-outfit',
+  		weights: [400],
+  		formats: ['woff']
+  	}
+  ]
 });
--- src/utils/generate-og-image.ts
+++ src/utils/generate-og-image.ts
@@ -1,34 +1,34 @@
-import { readFile } from "fs/promises";
+import { fontData } from "astro:assets";
 import type React from "react";
 import satori from "satori";
 import sharp from "sharp";
+import { readFile } from 'fs/promises'
 
 export async function generateOpenGraphImage(component: React.JSX.Element) {
+	const fontBarlowData = fontData['--font-barlow']
+	const fontOutfitData = fontData['--font-outfit']
+	const barlow = fontBarlowData.at(0)?.src.at(0)?.url
+	const outfit = fontOutfitData.at(0)?.src.at(0)?.url
+
 	const svg = await satori(component, {
 		width: 1200,
 		height: 630,
 		fonts: [
 			{
 				name: "Barlow",
-				data: await readFile(
-					"./node_modules/@fontsource/barlow/files/barlow-latin-400-normal.woff"
-				),
+				data: await readFile(`./dist${String(barlow)}`),
 				style: "normal",
 				weight: 400,
 			},
 			{
 				name: "Barlow",
-				data: await readFile(
-					"./node_modules/@fontsource/barlow/files/barlow-latin-700-normal.woff"
-				),
+				data: await readFile(`./dist${String(barlow)}`),
 				style: "normal",
 				weight: 700,
 			},
 			{
 				name: "Outfit",
-				data: await readFile(
-					"./node_modules/@fontsource/outfit/files/outfit-latin-400-normal.woff"
-				),
+				data: await readFile(`./dist${String(outfit)}`),
 				style: "normal",
 				weight: 400,
 			},
