4b95fb817c8a0e2d60185d657c12d795bfa919b1
Husen <hi@husen.id>
2026-06-14 22:46:05 +0700

    fix: can't build because wrong type from sharp's image
    Signed-off-by: Husen <hi@husen.id>

--- src/pages/og/article/[slug].png.ts
+++ src/pages/og/article/[slug].png.ts
@@ -59,7 +59,7 @@ export const GET: APIRoute = async ({ params }) => {
 		gravatarHash: author.gravatarHash
 	}));
 
-	return new Response(png, {
+	return new Response(new Uint8Array(png), {
 		headers: {
 			"Content-Type": "image/png",
 		},
